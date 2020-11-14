a939340ba7a50916c818eeb10426d542ac8bc280
cptpcrd <31829097+cptpcrd@users.noreply.github.com>
2020-11-14 08:26:45 -0500

    release: v0.1.1
    CHANGELOG:
    - fix: add missing function prototypes (fixes segfaults)
    - fix(notif): fix bugs in notification code
    - Improve test coverage

--- pyseccomp.py
+++ pyseccomp.py
@@ -6,7 +6,7 @@ import os
 import weakref
 from typing import Any, Collection, Iterable, Union, cast, overload
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 
 KILL_PROCESS = 0x80000000
 KILL_THREAD = 0x00000000
